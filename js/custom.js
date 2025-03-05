c15ebeb810b8500ad3a1a0bedf917d36e92f94d6
ucbg <hamidogs22@gmail.com>
2025-03-05 19:46:46 +0100

    Updated changes

--- js/custom.js
+++ js/custom.js
@@ -22,16 +22,6 @@ function search_animal() {
 }
 
 
-//ADsense
-(function() {
-  let script = document.createElement("script");
-  script.async = true;
-  script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7321073664976914";
-  script.crossOrigin = "anonymous";
-  document.head.appendChild(script);
-})();
-
-
 //Analytics
 function loadGoogleAnalytics(id) {
   // Google tag (gtag.js)
